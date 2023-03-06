a5662fcf3728d27df9e7eedaa48c2b7f53729897
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-03-06 00:44:05 -0300

    feat: config lib install and renomed title

---
+++ src/libs/dayjs.js
@@ -0,0 +1,6 @@
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import 'dayjs/locale/pt-br'
+
+dayjs.locale('pt-br')
+dayjs.extend(customParseFormat)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
+import './libs/dayjs'
 import App from './App.vue'
 import routes from './routes'
+import { vMaska } from "maska"
+import Vue3Toasity from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
-createApp(App).use(routes).mount('#app')
+createApp(App)
+  .use(routes)
+  .directive("maska", vMaska)
+  .use(Vue3Toasity, {
+    autoClose: 3000,
+    theme: "colored"
+  })
+  .mount('#app')
