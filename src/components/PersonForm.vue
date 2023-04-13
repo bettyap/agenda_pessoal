d7b55214eb921fbb8e00ed898de3b5601234aa5a
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-13 13:25:23 -0300

    feat: create component PersonForm,integration with api and add validations

---
+++ src/components/PersonForm.vue
@@ -0,0 +1,176 @@
+<template>
+  <form class="form" @submit="onSubmit">
+    <Input 
+      label="Adicionar foto" 
+      type="file" 
+      ref="fileInput"
+    />
+    <Input 
+      label="Nome Completo *" 
+      name="nome" 
+      v-model.trim="v$.nome.$model" 
+      :errors="v$.nome.$errors"
+    />
+    <Input 
+      label="País *" 
+      v-model.trim="v$.endereco.pais.$model"
+      :errors="v$.endereco.pais.$errors"
+    />
+    <Input 
+      label="CPF *" 
+      vMaska 
+      dataMaska="###.###.###-##" 
+      v-model="cpf" 
+      :errors="v$.cpf.$errors"
+    />
+    <Input 
+      label="CEP *" 
+      dataMaska="#####-###" 
+      v-model.trim="v$.endereco.cep.$model"
+      :errors="v$.endereco.cep.$errors"
+    />
+    <Input 
+      label="Cidade *" 
+      v-model.trim="v$.endereco.cidade.$model"
+      :errors="v$.endereco.cidade.$errors"
+    />
+    <Input 
+      label="Estado *" 
+      v-model="endereco.estado" 
+      v-model.trim="v$.endereco.estado.$model" 
+      :errors="v$.endereco.estado.$errors"
+    />
+    <Input 
+      label="Endereço *" 
+      v-model.trim="v$.endereco.logradouro.$model" 
+      :errors="v$.endereco.logradouro.$errors"
+    />
+    <Input 
+      label="Número *" 
+      v-model.trim="v$.endereco.numero.$model" 
+      :errors="v$.endereco.numero.$errors"
+    />
+    <Input 
+      label="Bairro *" 
+      v-model.trim="v$.endereco.bairro.$model" 
+      :errors="v$.endereco.bairro.$errors"
+    />
+
+    <!-- <DynamicInput 
+      msg="Cadastrar contato"
+    /> -->
+
+    <Button
+      title="Salvar"
+    />
+  </form>
+</template>
+
+<script>
+import api from '../services/api'
+import Button from './Button.vue'
+import DynamicInput from './DynamicInput.vue'
+import Input from './Input.vue'
+import useVuelidate from '@vuelidate/core'
+import { required, helpers } from '@vuelidate/validators'
+
+export default {
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  data() {
+    return {
+      nome: "",
+      cpf: "", 
+      endereco: {
+        bairro: "",
+        cep: "",
+        cidade: "",
+        estado: "",
+        logradouro: "",
+        numero: "",
+        pais: ""
+      }
+    }
+  },
+  validations () {
+    return {
+      nome: {required: helpers.withMessage('Este campo é obrigatório', required)},
+      cpf: {required: helpers.withMessage('Este campo é obrigatório', required)},
+      endereco: {
+        bairro: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        cep: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        cidade: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        estado: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        logradouro: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        numero: {required: helpers.withMessage('Este campo é obrigatório', required)},
+        pais: {required: helpers.withMessage('Este campo é obrigatório', required)},
+      }
+    }
+  },
+  components: { Input, DynamicInput, Button },
+  methods: {
+    async onSubmit(event) {
+      event.preventDefault()
+
+      this.v$.$touch(); 
+      if(this.v$.$invalid !== false) {
+        return 
+      }
+
+      let pessoa = await this.criarCadastroPessoa()
+      let upload = await this.onUpload(pessoa.data.object.id)
+      
+    },
+    async criarCadastroPessoa() {
+      let response = null
+
+      try {
+        response = await api.post('/pessoa/salvar', {
+          id: 4,
+          nome: this.nome,
+          cpf: this.cpf,
+          endereco: {
+            id: 7,
+            bairro: this.endereco.bairro,
+            cep:  this.endereco.cep,
+            cidade: this.endereco.cidade,
+            estado: this.endereco.estado,
+            logradouro: this.endereco.logradouro,
+            numero: this.endereco.numero,
+            pais: this.endereco.pais
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+      return response 
+    },
+
+    async onUpload(id) {
+      const formData = new FormData();
+      console.log(this.$refs.fileInput.$refs.input)
+      formData.append('foto', this.$refs.fileInput.$refs.input.files[0]);
+      
+      let response = null
+
+      try {
+        response = await api.post(`/foto/upload/${id}`, formData);
+        console.log('File uploaded:', response);
+      } catch (error) {
+        console.error(error);
+      }
+      return response
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .form {
+    max-width: 600px;
+    display: flex;
+    flex-direction: column;
+    margin: 1rem 0;
+  }
+</style>
