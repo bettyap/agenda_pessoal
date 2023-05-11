fb94ea4b4aa61ce0d01acd17cc3c9872abace493
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-05-11 10:35:45 -0300

    feat: using q-input search and debounce of quasar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container-search">
-    <input 
-      class="search_input"
-      type="search" 
+    <q-input
+      outlined
+      @update:model-value="$emit('change', $event)"
       placeholder="Buscar"
-      @input="onChangeDebounced"
-    />
+      debounce="500"
+      style="background: white; width: 100%"
+    >
+      <template v-slot:append>
+        <q-icon name="search" />
+      </template>
+    </q-input>
 
   </div>
 </template>
@@ -13,25 +18,17 @@
 <script>
   import { debounce } from '../utils/debounce.js'
 export default {
-  created() {
-    this.onChangeDebounced = debounce(event => {
-      this.$emit('change', event.target.value)
-    }, 500);
-  },
+  // created() {
+  //   this.onChangeDebounced = debounce(event => {
+  //     this.$emit('change', event.target.value)
+  //   }, 500);
+  // },
 }
 </script>
 
 <style>
   .container-search {
     width: 100%;
-    height: 2.14rem;
     display: flex;
-    justify-content: center;
-    border: 1px solid black 
-  }
-  .search_input {
-    width: 100%;
-    padding: 0.25rem;
-    border: none;
   }
 </style>
